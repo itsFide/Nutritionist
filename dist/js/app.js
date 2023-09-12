fa559732e27369dddca554d785eaf0205569e127
Sergey <murzak301@gmail.com>
2023-09-12 23:37:53 +0600

    finish firts page

---
+++ dist/js/app.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.testimonials__mobile', {
+    // Optional parameters
+    loop: true,
+    
+    spaceBetween: 40,
+    centeredSlides: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    breakpoints:{
+        
+        320:{
+            slidesPerView:1,
+        },
+        650:{
+            slidesPerView:1.5,
+        },
+    }
+  });
